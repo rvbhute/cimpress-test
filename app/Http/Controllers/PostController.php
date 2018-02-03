58389c42af398206a77a6a686df787b4cd6f2635
mum hrtest <mumhrtest@vistaprint.net>
2018-02-03 12:39:26 +0530

    db scaffolding

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    function viewNewPostForm()
+	{
+		//
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+	Route::get('posts', 'PostController@index');
+	Route::get('posts/new', 'PostController@viewNewPostForm');
+	Route::post('posts', 'PostController@saveNewPost');
+	Route::get('posts/{id}', 'PostController@viewPost');
+	Route::post('posts/{id}', 'PostController@savePost');
+});
